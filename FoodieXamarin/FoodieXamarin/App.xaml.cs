9516b6f32e5acc7d2da66bc26f45239247917d42
bornehill <ali.escamilla.navarro@gmail.com>
2019-04-09 11:56:02 -0500

    First commit Add TableView

---
+++ FoodieXamarin/FoodieXamarin/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using FoodieXamarin.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace FoodieXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new EditRecipePage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
