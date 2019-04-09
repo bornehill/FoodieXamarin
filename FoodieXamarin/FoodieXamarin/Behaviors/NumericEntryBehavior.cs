ead93bcbc6f5d94492c4299f3576527344579804
bornehill <ali.escamilla.navarro@gmail.com>
2019-04-09 12:17:51 -0500

    Added behavior

---
+++ FoodieXamarin/FoodieXamarin/Behaviors/NumericEntryBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FoodieXamarin.Behaviors
+{
+    public class NumericEntryBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += TextChangedHandler;
+        }
+
+        void TextChangedHandler(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+                return;
+
+            double val;
+            if (!double.TryParse(e.NewTextValue, out val))
+                ((Entry)sender).Text = e.OldTextValue;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= TextChangedHandler;
+        }
+    }
+}
