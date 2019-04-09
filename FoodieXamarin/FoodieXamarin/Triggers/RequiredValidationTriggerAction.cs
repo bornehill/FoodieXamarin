1c3330fcd80a815f52c4f3c444c39fb3bbb274bc
bornehill <ali.escamilla.navarro@gmail.com>
2019-04-09 12:59:17 -0500

    Added triggers

---
+++ FoodieXamarin/FoodieXamarin/Triggers/RequiredValidationTriggerAction.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace FoodieXamarin.Triggers
+{
+    public class RequiredValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            sender.BackgroundColor = string.IsNullOrEmpty(sender.Text) ?
+                Color.FromHex("FFCDD2") : Color.Default;
+        }
+    }
+}
